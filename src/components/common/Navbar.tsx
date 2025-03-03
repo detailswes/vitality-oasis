1f2db5f24f3f1e307b7623ebc20cd4cbeea10b44
detailswes <details.wes@gmail.com>
2025-03-03 18:01:29 +0530

    fixed header

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -68,62 +68,67 @@ export default function Navbar() {
   return (
     <header
       ref={headerRef}
-      className={`bg-white px-4 z-50 ${
-        isSticky
+      className={`bg-white px-4 z-50 ${isSticky
           ? "fixed top-0 left-0 right-0 shadow-md transition-all duration-300"
           : "relative"
-      }`}
+        }`}
     >
+
       <div className="max-w-[1200px] mx-auto flex items-center justify-between py-[14px]">
-        {/* Logo */}
+        {/* Left: Logo */}
         <a href="/" className="flex items-center">
           <img src={Logo.src} alt="Logo" className="w-[178px] sm:w-[242px]" />
         </a>
 
-        {/* Mobile Menu Toggle */}
-        <Button
-          ref={buttonRef}
-          className="bg-transparent text-black border md:hidden"
-          onClick={() => setOpen(!open)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </Button>
-
-        
-      </div>
-
-      {/* Mobile Menu */}
-      <div
-        ref={mobileMenuRef}
-        className={`md:hidden absolute inset-x-0 bg-white transition-transform duration-500 shadow-[0_33px_30px_rgba(0,0,0,0.2)] ${
-          open ? "translate-x-0" : "translate-x-[-800px]"
-        }`}
-      >
-        <nav className="flex flex-col items-start p-4 space-y-4">
-          {NAV_LINKS.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="hover:text-primary transition"
+        {/* Right: Help Icon & Text */}
+        <div className="flex items-center gap-2 ml-auto">
+        <a href={`tel:(855)-871-53321`}>
+            <div
+              className="flex py-3 px-8 rounded-[6px] bg-secondary hover:bg-primary text-text hover:text-white transition-colors w-full flex-col justify-center items-center gap-1"
             >
-              {link.name}
-            </a>
-          ))}
-        </nav>
+              <div className="w-full flex items-center justify-center gap-2">
+                <svg
+                  width="23"
+                  height="21"
+                  viewBox="0 0 23 21"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6.8786 1.55176C7.43615 1.55176 9.66637 6.166 9.66637 6.67869C9.66637 7.70408 7.9937 8.72946 7.43615 9.75485C6.8786 10.7802 7.9937 11.8056 9.10881 12.831C9.5437 13.2309 11.339 14.8818 12.4541 14.3691C13.5692 13.8564 14.6844 12.3183 15.7995 12.3183C16.357 12.3183 21.375 14.3691 21.375 14.8818C21.375 16.9326 19.7023 18.4706 18.0297 18.9833C16.357 19.496 15.2419 19.496 13.0117 18.9833C10.7815 18.4706 9.10881 17.9579 6.32104 15.3945C3.53327 12.831 2.97572 11.2929 2.41817 9.24216C1.86061 7.19138 1.86061 6.166 2.41817 4.62792C2.97572 3.08984 4.64838 1.55176 6.8786 1.55176Z"
+                    stroke="currentColor"
+                    stroke-width="2.23022"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"></path>
+                  <path
+                    d="M13.5692 5.69421C14.3052 6.01208 14.9743 6.44275 15.5318 6.96569C16.0782 7.46813 16.5354 8.07311 16.87 8.72936"
+                    stroke="currentColor"
+                    stroke-width="2.23022"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"></path>
+                  <path
+                    d="M14.6843 1.80798C16.2566 2.18738 17.6617 2.93591 18.7768 3.9613C19.8807 4.98668 20.6948 6.26842 21.0962 7.70396"
+                    stroke="currentColor"
+                    stroke-width="2.23022"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"></path>
+                </svg>
+
+                <p className="text-xl font-semibold">
+                Free Confidential Helpline
+                </p>
+              </div>
+              <p className="text-xl font-semibold">
+              (855)-871-53321
+              </p>
+            </div>
+          </a>
+        </div>
+
       </div>
+
+
+
     </header>
   );
 }
--- src/data/transformSectionData.ts
+++ src/data/transformSectionData.ts
@@ -1,30 +1,49 @@
+import room1 from "@/assets/images/image1.svg"
+import room2 from "@/assets/images/image2.svg"
+import room3 from "@/assets/images/image3.svg"
+import room4 from "@/assets/images/image4.svg"
+import room5 from "@/assets/images/image5.svg"
+import room6 from "@/assets/images/image6.svg"
+
+
 export const transformSectionData = {
     heading: "Transform Your Life: Discover Hope and Holistic Healing at Our Private Rehab Retreat",
     description:"Begin Your Journey to Lasting Sobriety at Kentucky's Leading Rehab Center. We provide a supportive, home-like environment paired with expert clinical care, empowering you to achieve lasting recovery and a fulfilling life beyond addiction.",
     images:[
         {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+            image:room1
         },
         {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+           
+            image:room2       
+         },
+        {
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+           
+            image:room3
         },
         {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+           
+            image:room4
         },
         {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+           
+            image:room1
         },
         {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
-        },
-        {
-            name:"Test1",
-            image:"https://files.housingbookers.com/properties/14/1739202148277-d0cofn0g7.jpg"
+            title:"Designed with your recovery at top of mind",
+            description:"Our design prioritizes your recovery, ensuring every detail is crafted with your comfort and well-being in mind. From thoughtful features to supportive elements, we are committed to providing you with a seamless and restorative experience.",
+           
+            image:room2
         }
     ]
 }
